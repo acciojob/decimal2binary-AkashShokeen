712cc1dbdb52bce88a96688f827f3eca4b7f7037
Akash Shokeen <117618540+AkashShokeen@users.noreply.github.com>
2023-08-16 13:21:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+ 
+const number = parseInt(num);
+
+const result = number.toString(2);
+
+console.log(result);
 }
 
 window.decimalToBinary = decimalToBinary;
